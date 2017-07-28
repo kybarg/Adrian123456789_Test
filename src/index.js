496cacee401d596af60cb96476c1e79de975859a
Ruslan Kyba <kybargr@gmail.com>
2017-07-28 03:26:24 +0300

    Fixed error handler

--- src/index.js
+++ src/index.js
@@ -14,6 +14,28 @@ function error(status, msg) {
   return err;
 }
 
+function challengeMe(error){
+	return Client.Web.Challenge.resolve(error)
+		.then(function(challenge){
+			// challenge instanceof Client.Web.Challenge
+			console.log(challenge.type);
+			// can be phone or email
+			// let's assume we got phone
+			if(!challenge.type !== 'phone') return;
+			//Let's check if we need to submit/change our phone number
+			return challenge.phone('+10123456789')
+		})
+		.then(function(challenge){
+			// Ok we got to the next step, the response code expected by Instagram
+			return challenge.code('123456');
+		})
+		.then(function(challenge){
+			// And we got the account confirmed!
+			// so let's login again
+			return loginAndFollow(device, storage, user, password);
+		})
+}
+
 let device, storage, InstagramSession, InstagramAccount
 
 app.get('/api/instagram/login', (req, res, next) => {
@@ -28,7 +50,7 @@ app.get('/api/instagram/login', (req, res, next) => {
     .catch(InstagramV1.Exceptions.CheckpointError, function(error){
       // Ok now we know that Instagram is asking us to
       // prove that we are real users
-      return next(error);
+      return challengeMe(error);
     }) 
     .then((session) => {
       InstagramSession = session
